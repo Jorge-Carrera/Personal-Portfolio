d44a59a3652d0ffc91837f100ce3c1b6b81415bb
Jorge Carrera <JCarrera994@gmail.com>
2023-01-17 23:09:17 -0500

    installs simple-typewriter and creates hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,60 @@
+import Image from "next/image";
+import React from "react";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
+import ProfilePicture from "../../public/Jorge_Carrera.jpeg"
+import Link from "next/link";
+
+type Props = {};
+
+function Hero({}: Props) {
+  const [text, count] = useTypewriter({
+    words: [
+      "Hi, My Name is Jorge",
+      "Prounounced as George",
+      "A-guy-who-loves-Gaming.tsx",
+      "<ButLovesToCodeMore />",
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
+
+  return (
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <div className="relative h-32 w-32 mx-auto">
+        <Image
+          src={ProfilePicture}
+          priority
+          alt="Jorge Carrera"
+          className="object-cover rounded-full"
+        />
+      </div>
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Software Engineer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10 ">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+        <div className="pt-5">
+          <Link href={"#about"}>
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href={"#experience"}>
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href={"#skills"}>
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href={"projects"}>
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,23 @@
 import Head from 'next/head'
 import Header from '@/components/Header'
+import Hero from '@/components/Hero'
 
 export default function Home() {
   return (
-    <>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
         <title>Jorge's Portfolio</title>
       </Head>
       <Header />
       {/* Hero section */}
+      <section id='hero' className='snap-center'>
+          <Hero />
+      </section>
       {/* About */}
       {/* Experience  */}
       {/* Skills */}
       {/* Projects */}
       {/* Contact */}
-    </>
+    </div>
   )
 }
