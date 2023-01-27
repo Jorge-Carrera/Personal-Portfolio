b8bfb0af8c884c7e0fd7b05d56fe463db692a908
Jorge Carrera <JCarrera994@gmail.com>
2023-01-27 01:00:53 -0500

    fixes hydration error by removing <Link> over social icon email

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -44,29 +44,28 @@ function Header({}: Props) {
         />
       </motion.div>
 
-      <Link href="#contact" className="buttonHover">
-        <motion.div
-          initial={{
-            x: 500,
-            opacity: 0,
-            scale: 0.5,
-          }}
-          animate={{
-            x: 0,
-            opacity: 1,
-            scale: 1,
-          }}
-          transition={{
-            duration: 1.5,
-          }}
-          className="flex flex-row items-center text-gray-300 cursor-pointer"
-        >
-          <SocialIcon network="email" fgColor="gray" bgColor="transparent" />
-          <p className="uppercase hidden md:inline-flex text-sm text-gray-400 ">
-            Get In Touch
-          </p>
-        </motion.div>
-      </Link>
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center text-gray-300 cursor-pointer"
+        onClick={() => (window.location.href = "#contact")}
+      >
+        <SocialIcon network="email" fgColor="gray" bgColor="transparent" />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400 ">
+          Get In Touch
+        </p>
+      </motion.div>
     </header>
   );
 }
