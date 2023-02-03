d12a0921f469525a971227b499a7eda6661c374e
Jorge Carrera <JCarrera994@gmail.com>
2023-02-02 22:29:47 -0500

    creates sanity schemas

--- src/components/Skills/Skill.tsx
+++ src/components/Skills/Skill.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 function Skill({ directionLeft }: Props) {
   return (
-    <div className="flex cursor-pointer">
+    <div className="group relative flex cursor-pointer">
       <motion.img
         initial={{
           x: directionLeft ? -200 : 200,
@@ -17,8 +17,13 @@ function Skill({ directionLeft }: Props) {
         viewport={{ once: true }}
         //placeholder image 
         src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-        className="rounded-full border border-gray-500 object-cover h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32"
+        className="rounded-full border border-gray-500 object-cover h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
       /> 
+      <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0 ' >
+        <div className='flex items-center justify-center h-full'>
+          <p className='text-3xl font-bold text-black opacity-100'>100%</p>
+        </div>
+      </div>
     </div>
   );
 }
